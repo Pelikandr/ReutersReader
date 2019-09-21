10e47f2e0a0a4092a1b24911f290095abc440a22
Denis Zayakin <>
2019-09-21 18:16:50 +0300

    added current date update

--- ReutersReader/ReutersReader/NameTableViewController.swift
+++ ReutersReader/ReutersReader/NameTableViewController.swift
@@ -13,11 +13,25 @@ class NameTableViewController: UITableViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
 
+    private lazy var dateFormatter = DateFormatter()
+
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateDate), userInfo: nil, repeats: true)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
+    }
+
+    @objc func updateDate() {
+        dateLabel.text = dateToString(from:Date())
+    }
+
+    private func dateToString(from date: Date) -> String {
+        dateFormatter.dateFormat = "dd-MM-yy, HH:mm:ss"
+        return dateFormatter.string(from: date)
     }
 }
